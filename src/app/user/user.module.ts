a41fa9a39f1a8deb4d81dc9ee48bee9382963c73
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-05-24 21:10:13 +0200

    #06.72 Exporting Components

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AuthModalComponent],
+  imports: [CommonModule],
+  exports: [AuthModalComponent],
 })
-export class UserModule { }
+export class UserModule {}
