84799be386b58439ca1c4b809714b4815005eed1
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-06-03 12:05:21 +0200

    #06.89 CSS Issues

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 
 @Component({
@@ -9,11 +9,13 @@ import { ModalService } from '../../services/modal.service';
 export class ModalComponent implements OnInit {
   @Input() modalID = '';
 
-  constructor(public modal: ModalService) {}
+  constructor(public modal: ModalService, public el: ElementRef) {}
 
   closeModal() {
     this.modal.toggleModal(this.modalID);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
+  }
 }
