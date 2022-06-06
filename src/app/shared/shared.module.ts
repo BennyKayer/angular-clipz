c0c71bdcd9b06277e6baf168e02e5b95f270e457
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-06-06 19:23:24 +0200

    #06.90 Creating a Tabs Component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
+import { TabComponent } from './tab/tab.component';
 
 @NgModule({
-  declarations: [ModalComponent],
+  declarations: [ModalComponent, TabsContainerComponent, TabComponent],
   imports: [CommonModule],
-  exports: [ModalComponent],
+  exports: [ModalComponent, TabsContainerComponent, TabComponent],
 })
 export class SharedModule {}
