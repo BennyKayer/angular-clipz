ff3fc0b1f5fc49166f30c4519fcb2c86d68573c0
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-06-15 15:02:38 +0200

    #07.99 Registering a new Form

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class RegisterComponent {
+  registerForm = new FormGroup({});
 }
