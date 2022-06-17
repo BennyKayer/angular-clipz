cd52fad97ab32b27c0b33d6c270b5cbcbf8bfddd
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-06-17 14:33:58 +0200

    #07.102 Form Validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,7 +8,7 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class RegisterComponent {
   registerForm = new FormGroup({
-    name: new FormControl(''),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     email: new FormControl(''),
     age: new FormControl(''),
     password: new FormControl(''),
