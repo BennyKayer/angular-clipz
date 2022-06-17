15fa1d5e96c3702bb0e8dd3e3c44d8345d32f5ba
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-06-17 20:16:39 +0200

    #07.116 Designing an Alert Component

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -34,7 +34,13 @@ export class RegisterComponent {
     phoneNumber: this.phoneNumber,
   });
 
+  showAlert = false;
+  alertMsg = 'Please wait! Your account is being created';
+  alertColor = 'blue';
+
   register() {
-    console.log('reg');
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! Your account is being created';
+    this.alertColor = 'blue';
   }
 }
