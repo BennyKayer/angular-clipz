8fe33ef414ca5add8a01b7dccddd52a8bd50997e
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-06-18 10:48:33 +0200

    #08.119 Two Way Binding

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  credentials = {
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
