ba72ae773cefca282d1ee199badf4dc5f4cac41c
Paweł Benkowski <pawlo97.pb@gmail.com>
2022-06-18 10:28:25 +0200

    #08.117 Importing FormsModule

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,10 +5,11 @@ import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AuthModalComponent, LoginComponent, RegisterComponent],
-  imports: [CommonModule, SharedModule, ReactiveFormsModule],
+  imports: [CommonModule, SharedModule, ReactiveFormsModule, FormsModule],
   exports: [AuthModalComponent],
 })
 export class UserModule {}
